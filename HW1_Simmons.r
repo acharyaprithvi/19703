412e99b84cad8a3bdd86f95edbf02b511fd9d2d2
acharyaprithvi <35804686+acharyaprithvi@users.noreply.github.com>
2018-01-25 08:26:45 -0500

    Update HW1_Simmons.r

--- HW1_Simmons.r
+++ HW1_Simmons.r
@@ -11,21 +11,20 @@ sig.count.01 <- 0
 #using these variables to count how many simulations have results 
 #that are significant (at p = 0.1, 0.05,and 0.01 respectively).
 
-for(i in 1:no.of.sims)
-{
+for(i in 1:no.of.sims) {
   set.seed(i) #by setting a seed for each random variable pull
               #I am ensuring reproducibiity of results.
-  liking <- rnorm(20,1,1)
-  set.seed(2*i) #using the same seed for the two sets of variables will
+  liking <- rnorm(20, 1, 1)
+  set.seed(1500 + i) #using the same seed for the two sets of variables will
                 #generate identical vectors with Cov(x,y) = 1, making
                 #the whole t-test meaningless.
-   wtp <- rnorm(20,1,1)
+   wtp <- rnorm(20, 1, 1)
    p.20 <- t.test(liking, y = wtp)$p.value #p-value for first twenty
    
-   set.seed(3*i)
-   liking <- c(liking, rnorm(10,1,1)) #adding 10 observations
-   set.seed(4*i)
-   wtp <- c(wtp, rnorm(10,1,1)) 
+   set.seed(3000 + i)
+   liking <- c(liking, rnorm(10, 1, 1)) #adding 10 observations
+   set.seed(4500 + i)
+   wtp <- c(wtp, rnorm(10, 1, 1)) 
    #reminder: this is ALL noise - the names "liking" and "wtp" are
    #used because they were mentioned in passing by Simmons et al. (2011)
    #and the mean and SD of the "rnorm" fns. was also arbitrary.
@@ -41,15 +40,15 @@ for(i in 1:no.of.sims)
    #collecting ten more instances to see how that affects the p-value.
 }
 
-sigs <- c(sig.count.10,sig.count.05,sig.count.01)
+sigs <- c(sig.count.10, sig.count.05, sig.count.01)
 sigs.percent <- as.data.frame(paste0(signif((
-  sigs/no.of.sims)*100,digits = 2),"%"))
+  sigs / no.of.sims) * 100, digits = 2), " %"))
 #converting the counts to % numbers
 
 #next few lines are string manipulation to generate the results table
 #no math to see here.
-sig.levels <- as.data.frame(c("p < .1","p  < .05", "p < .01"))
-result <- cbind(sig.levels,sigs.percent)
+sig.levels <- as.data.frame(c("p < .1", "p  < .05", "p < .01"))
+result <- cbind(sig.levels, sigs.percent)
 colnames(result) <- c("Signif. Levels", "% w/ Signif.")
 
 cat("\014") #clear screen
